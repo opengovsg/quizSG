d5fa13327734966d1d0cca8e17d07d3ab268ecac
Huang Kaiwen <huangkaiw3n@gmail.com>
2022-01-25 15:59:40 +0800

    chore: add default landing page with take quiz page scaffolding

--- frontend/src/app/AppRouter.tsx
+++ frontend/src/app/AppRouter.tsx
@@ -1,24 +1,23 @@
 import { lazy, Suspense } from 'react'
 import { Route, Switch } from 'react-router-dom'
 
-import { LOGIN_ROUTE, ROOT_ROUTE } from '~constants/routes'
+import { CREATOR_ROUTE, ROOT_ROUTE } from '~constants/routes'
 
-import { PrivateRoute } from './PrivateRoute'
 import { PublicRoute } from './PublicRoute'
 
-const WorkspacePage = lazy(() => import('~features/dashboard/DashboardPage'))
-const LoginPage = lazy(() => import('~features/auth/LoginPage'))
+const LandingPage = lazy(() => import('~pages/LandingPage'))
+const CreatorLandingPage = lazy(() => import('~pages/CreatorLandingPage'))
 
 export const AppRouter = (): JSX.Element => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Switch>
-        <PublicRoute exact path={LOGIN_ROUTE}>
-          <LoginPage />
+        <PublicRoute path={ROOT_ROUTE} strict={false}>
+          <LandingPage />
         </PublicRoute>
-        <PrivateRoute exact path={ROOT_ROUTE}>
-          <WorkspacePage />
-        </PrivateRoute>
+        <PublicRoute exact path={CREATOR_ROUTE} strict={false}>
+          <CreatorLandingPage />
+        </PublicRoute>
         <Route path="*">
           <div>404</div>
         </Route>
---
+++ frontend/src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,43 @@
+import { useHistory } from 'react-router-dom'
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+} from '@chakra-ui/react'
+
+import { ROOT_ROUTE } from '~constants/routes'
+
+const LandingPage = (): JSX.Element => {
+  const history = useHistory()
+
+  const handleSubmit = (e: any) => {
+    // Prevent default which reloads the page
+    // Instead call the primary event
+    e.preventDefault()
+    const quizId = e.target[0].value
+    history.push(`${ROOT_ROUTE}${quizId}`)
+  }
+  return (
+    <Flex flexDir="column">
+      Default Landing Page
+      <form onSubmit={handleSubmit}>
+        <FormControl>
+          <FormLabel htmlFor="quiz-id">Quiz ID</FormLabel>
+          <Input id="quiz-id" type="text" />
+          <FormHelperText>
+            Enter quiz ID of the quiz you want to take
+          </FormHelperText>
+        </FormControl>
+        <Button width="full" mt={4} type="submit">
+          Enter Quiz
+        </Button>
+      </form>
+    </Flex>
+  )
+}
+
+// Required to be default due to using dynamic import for lazy loading.
+export default LandingPage
