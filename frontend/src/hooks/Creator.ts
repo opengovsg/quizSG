aa37da20b361a89b6f0d7d0dc0ef1334d6ad82a6
Huang Kaiwen <huangkaiw3n@gmail.com>
2022-01-26 16:18:16 +0800

    feat: basic hooks for creator

---
+++ frontend/src/hooks/Creator.ts
@@ -0,0 +1,47 @@
+import { useMutation, useQuery } from 'react-query'
+
+import QuizApi, { QuizCreationDto } from '~services/QuizApi'
+
+const ALL_QUIZZES_QUERY_KEY = ['all_quizzes']
+
+export default function useFetchAllQuizzes() {
+  const {
+    data: response,
+    refetch,
+    status,
+    isFetching,
+    error,
+  } = useQuery(ALL_QUIZZES_QUERY_KEY, QuizApi.fetchAllQuizzes)
+
+  return {
+    allQuizzes: response,
+    fetchAllQuizzes: refetch,
+    fetchAllQuizzesStatus: status,
+    isFetchingAllQuizzes: isFetching,
+    fetchAllQuizzesError: error,
+  }
+}
+
+export function useCreateQuiz() {
+  const { mutateAsync, error, isLoading } = useMutation(
+    (quiz: QuizCreationDto) => QuizApi.createQuiz(quiz),
+  )
+
+  return {
+    createQuiz: mutateAsync,
+    createQuizError: error,
+    isCreateQuizLoading: isLoading,
+  }
+}
+
+export function useDeleteQuiz() {
+  const { mutateAsync, error, isLoading } = useMutation((id: string) =>
+    QuizApi.deleteQuiz({ id }),
+  )
+
+  return {
+    createQuiz: mutateAsync,
+    createQuizError: error,
+    isCreateQuizLoading: isLoading,
+  }
+}
