3a42eff8b3822ee5f0fca182a9c70f18f40cba53
Huang Kaiwen <huangkaiw3n@gmail.com>
2022-01-26 17:27:38 +0800

    chore: add eslint ignore to creator hook file

--- frontend/src/hooks/Creator.ts
+++ frontend/src/hooks/Creator.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { useMutation, useQuery } from 'react-query'
 
 import QuizApi, { QuizCreationDto } from '~services/QuizApi'
