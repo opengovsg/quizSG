df74c7e46a6f475efd66e9378b51a1223afcd086
Huang Kaiwen <huangkaiw3n@gmail.com>
2022-01-27 19:49:21 +0800

    feat: basic create quiz right column form (#55)
    * chore: install formik
    
    * chore: basic structure for quiz creation page
    
    * chore: update title to QuizSG
    
    * feat: basic quiz configuration form on right column

--- frontend/src/pages/CreatorLandingPage/CreateQuizPage/CreateQuizPage.tsx
+++ frontend/src/pages/CreatorLandingPage/CreateQuizPage/CreateQuizPage.tsx
@@ -1,14 +1,35 @@
-import { Flex, Heading, HStack } from '@chakra-ui/react'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState } from 'react'
+import { Box, GridItem, SimpleGrid, VStack } from '@chakra-ui/react'
+
+import Header from '~components/Header'
+
+import QuizConfigurationForm from './QuizConfigurationForm'
+
+const DEFAULT_QUIZ_CONFIG = {
+  quizName: '',
+  organisationName: '',
+  passingScore: '',
+  quizDescription: '',
+}
 
 const CreateQuizPage = (): JSX.Element => {
+  const [quizConfig, setQuizConfig] = useState(DEFAULT_QUIZ_CONFIG)
+  const [questions, setQuestions] = useState([])
   return (
-    <Flex flexDir="column" alignItems="center">
-      <Heading as="h1">New Quiz</Heading>
-      <HStack>
-        <div>New Question</div>
-        <div>Create Quiz</div>
-      </HStack>
-    </Flex>
+    <VStack bg="primary.100" alignItems="stretch">
+      <Header>New Quiz</Header>
+      <SimpleGrid columns={12} spacing={5} style={{ padding: 10 }}>
+        <GridItem bg="lightblue" colSpan={8}>
+          New question
+        </GridItem>
+        <GridItem bg="aliceblue" colSpan={4}>
+          <Box bg="white" style={{ borderRadius: 20, padding: 20 }}>
+            <QuizConfigurationForm quizConfig={quizConfig} />
+          </Box>
+        </GridItem>
+      </SimpleGrid>
+    </VStack>
   )
 }
 
