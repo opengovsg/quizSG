f721ef083da4adfeb2195145d4b434b3a2089a3b
Huang Kaiwen <huangkaiw3n@gmail.com>
2022-01-28 07:18:08 +0800

    feat: basic mcq-1 responsive

--- frontend/src/pages/CreatorLandingPage/CreateQuizPage/CreateQuizPage.tsx
+++ frontend/src/pages/CreatorLandingPage/CreateQuizPage/CreateQuizPage.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Box, Container, GridItem, SimpleGrid, VStack } from '@chakra-ui/react'
+import _ from 'lodash'
 
 import Header from '~components/Header'
 import NewQuestion from '~components/NewQuestion'
 import QuestionField from '~components/QuestionField'
 
+import Question from './Question'
 import QuizConfigurationForm from './QuizConfigurationForm'
 
 const DEFAULT_QUIZ_CONFIG = {
@@ -15,19 +18,99 @@ const DEFAULT_QUIZ_CONFIG = {
   quizDescription: '',
 }
 
+const COMMON_FIELDS = {
+  text: '',
+  details: '',
+  explanation: '',
+  pointValue: '',
+}
+
+// Exactly 1 option must be true with minimum 2 options
+const DEFAULT_MCQ_1 = {
+  ...COMMON_FIELDS,
+  type: 'MCQ-1',
+  options: [
+    {
+      text: '',
+      isTrue: true,
+    },
+    {
+      text: '',
+      isTrue: false,
+    },
+  ],
+}
+
+// Minimum 2 options
+const DEFAULT_MCQ_M = {
+  ...COMMON_FIELDS,
+  type: 'MCQ-M',
+  options: [
+    {
+      text: '',
+      isTrue: false,
+    },
+    {
+      text: '',
+      isTrue: false,
+    },
+  ],
+}
+
+// ============== SEE THIS!!! ======================
+// NOTE: Somehow, deleting questions and options is buggy.
+// When deleting the latest to the earliest it seems to work fine, but the reverse
+// seems to bug out.
 const CreateQuizPage = (): JSX.Element => {
   const [quizConfig, setQuizConfig] = useState(DEFAULT_QUIZ_CONFIG)
-  const [questions, setQuestions] = useState([])
+  const [questions, setQuestions] = useState<any>([])
+
+  const onClickNewQuestion = (questionType: string) => {
+    if (questionType === DEFAULT_MCQ_M.type) {
+      setQuestions((prevQuestions: any) => [
+        ...prevQuestions,
+        _.cloneDeep(DEFAULT_MCQ_M),
+      ])
+    }
+    if (questionType === DEFAULT_MCQ_1.type) {
+      setQuestions((prevQuestions: any) => [
+        ...prevQuestions,
+        _.cloneDeep(DEFAULT_MCQ_1),
+      ])
+    }
+  }
+
+  const onDeleteQuestion = (index: string) => {
+    setQuestions((prevQuestions: any) => {
+      console.log('splicing', parseInt(index), 1)
+      prevQuestions.splice(parseInt(index), 1)
+      return [...prevQuestions]
+    })
+  }
+
+  console.log('questions', questions)
+
   return (
     <Box bg="primary.100" alignItems="stretch" minH="100vh">
       <Header>New Quiz</Header>
       <Container maxW="container.xl" my={12}>
         <SimpleGrid columns={12} spacing={5} style={{ padding: 10 }}>
           <GridItem colSpan={8}>
-            <NewQuestion />
-            <QuestionField />
+            {_.isEmpty(questions)
+              ? null
+              : _.map(questions, (question, index) => (
+                  <Question
+                    key={index}
+                    index={index}
+                    question={question}
+                    onDelete={onDeleteQuestion}
+                    setQuestions={setQuestions}
+                  />
+                ))}
+            <NewQuestion onSubmit={onClickNewQuestion} />
+            {/* <QuestionField /> */}
           </GridItem>
-          <GridItem bg="aliceblue" colSpan={4}>
+          <GridItem colSpan={4}>
             <Box bg="white" p={6} borderRadius="lg" boxShadow="sm">
               <QuizConfigurationForm quizConfig={quizConfig} />
             </Box>
---
+++ frontend/src/pages/CreatorLandingPage/CreateQuizPage/Question.tsx
@@ -0,0 +1,209 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import { DeleteIcon } from '@chakra-ui/icons'
+import { Box, Flex, RadioGroup, Text } from '@chakra-ui/react'
+import { Button, Input, Radio, Textarea } from '@opengovsg/design-system-react'
+import _ from 'lodash'
+
+type Option = {
+  text: string
+  isTrue: boolean
+}
+
+type QuestionBase = {
+  details: string
+  explanation: string
+  options: Option[]
+  pointValue: string
+  text: string
+  type: 'MCQ-1' | 'MCQ-M'
+}
+
+const Question = ({
+  index,
+  question,
+  onDelete,
+  setQuestions,
+}: {
+  index: string
+  question: QuestionBase
+  onDelete: Function
+  setQuestions: Function
+}): JSX.Element => {
+  return (
+    <Flex mt={12} color="primary.900">
+      <Text textStyle="h1">{index + 1}.</Text>
+      <Box pl={8} pr={2} py={2} flex="1">
+        <Flex justifyContent="space-between">
+          <Text textStyle="subhead-1" py={2}>
+            Question Type:{' '}
+            <b>{question.type === 'MCQ-1' ? 'Multiple Choice' : 'Select'}</b>
+          </Text>
+          {/* delete question */}
+          <Button colorScheme="danger" onClick={() => onDelete(index)}>
+            <DeleteIcon />
+          </Button>
+        </Flex>
+        <Box py={1}>
+          <Text textStyle="subhead-1" py={2}>
+            Question Point Value
+          </Text>
+          <Input
+            placeholder="E.g 4"
+            value={question.pointValue}
+            onChange={(val) => {
+              setQuestions((prevQuestions: QuestionBase[]) => {
+                prevQuestions[parseInt(index)].pointValue = val.target.value
+                return [...prevQuestions]
+              })
+            }}
+          />
+        </Box>
+        <Box py={1}>
+          <Text textStyle="subhead-1" py={2}>
+            Question Name
+          </Text>
+          <Input
+            placeholder="E.g What is 1 + 1?"
+            value={question.text}
+            onChange={(val) => {
+              setQuestions((prevQuestions: QuestionBase[]) => {
+                prevQuestions[parseInt(index)].text = val.target.value
+                return [...prevQuestions]
+              })
+            }}
+          />
+        </Box>
+        <Box py={1}>
+          <Text textStyle="subhead-1" py={2}>
+            Additional Details
+          </Text>
+          <Textarea
+            placeholder="Additional Information for the question"
+            minAutosizeRows={5}
+            value={question.details}
+            onChange={(val) => {
+              setQuestions((prevQuestions: QuestionBase[]) => {
+                prevQuestions[parseInt(index)].details = val.target.value
+                return [...prevQuestions]
+              })
+            }}
+          />
+        </Box>
+        <Box py={1}>
+          <Text textStyle="subhead-1" py={2}>
+            Answer Explanation
+          </Text>
+          <Textarea
+            placeholder="Additional information and explanation for the answer"
+            minAutosizeRows={5}
+            value={question.explanation}
+            onChange={(val) => {
+              setQuestions((prevQuestions: QuestionBase[]) => {
+                prevQuestions[parseInt(index)].explanation = val.target.value
+                return [...prevQuestions]
+              })
+            }}
+          />
+        </Box>
+        {/* Radio Answer inputs */}
+        <Box mt={2}>
+          <Text textStyle="subhead-1" py={2}>
+            Answers (select the correct answer)
+          </Text>
+          <Box>
+            <RadioGroup
+              onChange={(val) => {
+                console.log('radio val', val)
+                setQuestions((prevQuestions: QuestionBase[]) => {
+                  const newOptions = _.map(
+                    prevQuestions[parseInt(index)].options,
+                    (option, index) => {
+                      return {
+                        ...option,
+                        isTrue: index === parseInt(val),
+                      }
+                    },
+                  )
+                  prevQuestions[parseInt(index)].options = newOptions
+                  return [...prevQuestions]
+                })
+              }}
+              value={_.findIndex(question.options, { isTrue: true }).toString()}
+            >
+              {question.options.map((el, optionIndex) => {
+                const optionValue = optionIndex.toString()
+                return (
+                  <Box my={2} key={optionValue}>
+                    <Flex alignItems="center">
+                      <Radio value={optionValue}>
+                        Answer {optionIndex + 1}
+                      </Radio>
+                      <Button
+                        color="primary.700"
+                        variant="reverse"
+                        bg="primary.100"
+                        border="none"
+                        _hover={{
+                          background: 'primary.100',
+                          color: 'danger.500',
+                        }}
+                        onClick={() => {
+                          setQuestions((prevQuestions: QuestionBase[]) => {
+                            prevQuestions[parseInt(index)].options.splice(
+                              optionIndex,
+                              1,
+                            )
+                            return [...prevQuestions]
+                          })
+                        }}
+                      >
+                        <DeleteIcon />
+                      </Button>
+                    </Flex>
+                    <Input
+                      placeholder="Type your answer"
+                      value={el.text}
+                      onChange={(val) => {
+                        setQuestions((prevQuestions: QuestionBase[]) => {
+                          prevQuestions[parseInt(index)].options[optionIndex] =
+                            {
+                              ...prevQuestions[parseInt(index)].options[
+                                optionIndex
+                              ],
+                              text: val.target.value,
+                            }
+                          return [...prevQuestions]
+                        })
+                      }}
+                    />
+                  </Box>
+                )
+              })}
+            </RadioGroup>
+          </Box>
+        </Box>
+        <Button
+          isFullWidth
+          my={8}
+          variant="outline"
+          onClick={() => {
+            setQuestions((prevQuestions: QuestionBase[]) => {
+              prevQuestions[parseInt(index)].options = [
+                ...prevQuestions[parseInt(index)].options,
+                {
+                  text: '',
+                  isTrue: false,
+                },
+              ]
+              return [...prevQuestions]
+            })
+          }}
+        >
+          Add Answer Field
+        </Button>
+      </Box>
+    </Flex>
+  )
+}
+
+export default Question
