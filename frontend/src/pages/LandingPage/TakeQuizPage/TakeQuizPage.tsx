8f35e46b15e88dac05025beec415933025e8fdf4
Herman <61788143+hurrmun@users.noreply.github.com>
2022-01-27 10:33:56 +0800

    Changed styles for Quiz Taker Page and added question and MCQAnswerGroup component (#47)
    * style(landing page): edited styles of Quiz Landing page
    
    * style(name): minor fix to add name text
    
    * style(question page and answer components): added Question page as a component and MCQ(4) answers
    
    Answer box is inside its own component while the question page itself is a component that contains
    the MCQ answer box

--- frontend/src/pages/LandingPage/TakeQuizPage/TakeQuizPage.tsx
+++ frontend/src/pages/LandingPage/TakeQuizPage/TakeQuizPage.tsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import {
   Box,
-  Button,
+  Container,
   Flex,
   Grid,
-  HStack,
+  GridItem,
   Input,
   Text,
   VStack,
 } from '@chakra-ui/react'
-// import {} from '@chakra-ui/icons'
+import { Button } from '@opengovsg/design-system-react'
+
+import QuestionPage from './components/QuestionPage'
 
 const TakeQuizPage = (): JSX.Element => {
   const { quizId } = useParams<{ quizId: string }>()
@@ -37,8 +39,11 @@ const TakeQuizPage = (): JSX.Element => {
 
   return (
     <>
-      <Header />
-      <LandingPage name={name} setName={setName} />
+      <Box bg="primary.100" h="100vh">
+        <Header />
+        <LandingPage name={name} setName={setName} />
+        <QuestionPage />
+      </Box>
     </>
   )
 }
@@ -50,10 +55,50 @@ const LandingPage = ({
   name: string
   setName: (name: string) => void
 }): JSX.Element => (
-  <Grid minH="100vh">
-    <Flex padding="100px 200px" background="gray.50" justify="space-between">
-      <Box h="50%" w="30%" bg="gray.400" flex={1} marginRight={20} />
-      <VStack flex={1} align="stretch">
+  // <Grid minH="100vh">
+  //   <Flex padding="100px 200px" background="gray.50" justify="space-between">
+  //     <Box h="50%" w="30%" bg="gray.400" flex={1} marginRight={20} />
+  //     <VStack flex={1} align="stretch">
+  //       <Text>
+  //         The Nationanl heritage of singpaore Lorem ipsum dolor sit amet,
+  //         consectetur adipiscing elit. Proin non lacinia leo, at laoreet arcu.
+  //         Orci varius natoque penatibus et magnis dis parturient montes,
+  //         nascetur ridiculus mus. Sed gravida sem a augue accumsan dictum.
+  //         Vivamus pellentesque odio eget nibh vehicula consectetur. Etiam at
+  //         porta purus, tempus accumsan lacus. Nam nec pellentesque erat. Donec
+  //         ut erat ut dui tempus aliquet a a augue. Etiam fermentum imperdiet
+  //         ligula, at tincidunt diam elementum vel.
+  //       </Text>
+  //       <Text fontWeight="bold">Your name</Text>
+  //       <HStack>
+  //         <Input
+  //           required
+  //           value={name}
+  //           placeholder="E.g. Tim"
+  //           type="text"
+  //           onChange={(e) => {
+  //             setName(e.target.value)
+  //           }}
+  //         />
+  //         <Button
+  //           colorScheme="primary"
+  //           type="submit"
+  //           onSubmit={(e) => {
+  //             e.preventDefault()
+  //           }}
+  //         >
+  //           Take Quiz →
+  //         </Button>
+  //       </HStack>
+  //     </VStack>
+  //   </Flex>
+  // </Grid>
+  <Container maxW="container.xl">
+    <Grid templateColumns="repeat(2, 1fr)" gap={6} alignItems="center" mt={20}>
+      <GridItem>
+        <Box h="400px" w="100%" bg="gray.400" flex={1} marginRight={20} />
+      </GridItem>
+      <GridItem>
         <Text>
           The Nationanl heritage of singpaore Lorem ipsum dolor sit amet,
           consectetur adipiscing elit. Proin non lacinia leo, at laoreet arcu.
@@ -64,30 +109,29 @@ const LandingPage = ({
           ut erat ut dui tempus aliquet a a augue. Etiam fermentum imperdiet
           ligula, at tincidunt diam elementum vel.
         </Text>
-        <Text fontWeight="bold">Your name</Text>
-        <HStack>
-          <Input
-            required
-            value={name}
-            placeholder="E.g. Tim"
-            type="text"
-            onChange={(e) => {
-              setName(e.target.value)
-            }}
-          />
-          <Button
-            colorScheme="primary"
-            type="submit"
-            onSubmit={(e) => {
-              e.preventDefault()
-            }}
-          >
-            Take Quiz →
-          </Button>
-        </HStack>
-      </VStack>
-    </Flex>
-  </Grid>
+        <Box mt={10}>
+          <Text fontWeight="bold" textStyle="subhead-1">
+            Your name
+          </Text>
+          <Flex mt={2}>
+            <Input
+              required
+              value={name}
+              placeholder="E.g. Tim"
+              type="text"
+              onChange={(e) => {
+                setName(e.target.value)
+              }}
+              mr={2}
+            />
+            <Button colorScheme="primary" type="submit">
+              Take Quiz →
+            </Button>
+          </Flex>
+        </Box>
+      </GridItem>
+    </Grid>
+  </Container>
 )
 
 // Required to be default due to using dynamic import for lazy loading.
