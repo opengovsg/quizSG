8004f2fd97f00c17572c1b584058e00cb510f50a
Jerald Yik <43469989+JeraldYik@users.noreply.github.com>
2022-02-23 16:06:54 +0800

    feat: firm up generate weblink alert modal (#79)
    Co-authored-by: Jerald <jerald@Jeralds-MacBook-Pro.local>

--- frontend/src/pages/CreatorLandingPage/CreatorQuizSummaryPage/utils.ts
+++ frontend/src/pages/CreatorLandingPage/CreatorQuizSummaryPage/utils.ts
@@ -1,2 +1,13 @@
 export const convertDecimalToPercent = (decimal: string): string =>
   `${Math.round(parseFloat(decimal) * 100)}%`
+
+export const generateWeblinkDisplay = (quizId: string): void => {
+  const url = `${window.location.origin}/${quizId}`
+  if (
+    window.confirm(
+      `Here is the link to the quiz for taker: ${url}. Click "OK" to open the quiz in a new tab, or "CANCEL" to not do so.`,
+    )
+  ) {
+    window.open(url)
+  }
+}
